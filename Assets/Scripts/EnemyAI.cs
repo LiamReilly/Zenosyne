4fdf7f27eed3b012e506b02c5b5ed0537b3071b3
Bryan Erazo <bryanerazo29@gmail.com>
2020-12-19 03:19:25 -0500

    Added EnemyAI basic functions

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,9 +1,48 @@
+using System;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace DefaultNamespace
 {
     public class EnemyAI : MonoBehaviour
     {
-        
+        [SerializeField] private Transform target;
+        [SerializeField] private float chaseRange = 10f;
+        [SerializeField] private float attackRange = 6f;
+        private NavMeshAgent _navMeshAgent;
+        private Animation _animation; 
+        private float _distanceToTarget = Mathf.Infinity;
+        void Start()
+        {
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
+        void Update()
+        {
+            _distanceToTarget = Vector3.Distance(target.position, transform.position);
+
+            if (_distanceToTarget <= chaseRange)
+            {
+                chaseTarget();
+            }
+
+            if (_distanceToTarget <= attackRange)
+            {
+                attackTarget();
+            }
+
+        }
+
+        void chaseTarget()
+        {
+            _navMeshAgent.isStopped = false; 
+            _navMeshAgent.SetDestination(target.position);
+        }
+
+        void attackTarget()
+        {
+            _navMeshAgent.isStopped = true;
+            print("attack");
+        }
     }
 }
